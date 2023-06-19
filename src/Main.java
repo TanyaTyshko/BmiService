8755c1bed663f8a7e87ab82d8fe2046c39aaf428
Tanya <Tanja_33@mail.ru>
2023-06-19 17:19:27 +0700

    Gitignore added

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        BmiService service = new BmiService();
+        int weightInKg = 98;
+        double heightInMeters = 1.87;
+        int index = service.calculate(weightInKg, heightInMeters);
+        System.out.println(index);
+
+
+    }
+}
